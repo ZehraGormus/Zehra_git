1ae56d7cd719dad3061e1d3694d5b3bd954b54a6
ZehraGormus <sayilarla.34@gmail.com>
2022-03-17 19:03:08 +0100

    All project files uploaded

---
+++ src/main/java/Day1.java
@@ -0,0 +1,6 @@
+public class Day1 {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+        System.out.println("Hello 2. defa guncelleme");
+    }
+}
---
+++ src/test/java/day2.java
@@ -0,0 +1,7 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class day2 {
+    public static void main(String[] args) {
+        System.out.println("heloo");
+    }
+}
