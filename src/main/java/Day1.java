6461402b149370a980fc5f8fbbe886519fc72447
ZehraGormus <sayilarla.34@gmail.com>
2022-03-17 20:09:35 +0100

    last changes commit project files uploaded

--- src/main/java/Day1.java
+++ src/main/java/Day1.java
@@ -2,5 +2,7 @@ public class Day1 {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("Hello 2. defa guncelleme");
+        //commit sonrasi degisiklik yapildi
+git
     }
 }
--- src/test/java/day2.java
+++ src/test/java/day2.java
@@ -3,5 +3,6 @@ import org.w3c.dom.ls.LSOutput;
 public class day2 {
     public static void main(String[] args) {
         System.out.println("heloo");
+        //commit sonrasi degisiklik yapildi
     }
 }
