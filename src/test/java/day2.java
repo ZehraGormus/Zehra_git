3bcfda9781eb5a80685e444eca1fe5738b4665db
ZehraGormus <101753225+ZehraGormus@users.noreply.github.com>
2022-03-18 10:35:08 +0100

    Update day2.java

--- src/test/java/day2.java
+++ src/test/java/day2.java
@@ -4,5 +4,7 @@ public class day2 {
     public static void main(String[] args) {
         System.out.println("heloo");
         //commit sonrasi degisiklik yapildi
+        
+        //Github sonrasi degisiklik (odev)
     }
 }
